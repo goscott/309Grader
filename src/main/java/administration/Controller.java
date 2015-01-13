d5abd37196173308f953132faa1d87f3cfc4ecd4
Mason Stevenson <masonjstevenson@gmail.com>
2015-01-12 19:52:25 -0800

    Added permissions editor fxml view

---
+++ src/main/java/administration/Controller.java
@@ -0,0 +1,17 @@
+package administration;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
+public class Controller {
+    
+    @FXML
+    private Button edit_button;
+    
+    @FXML
+    private ListView<String> user_list;
+    
+    public void method() {
+        
+    }
+}
--- src/main/java/administration/dbtest.java
+++ src/main/java/administration/dbtest.java
@@ -4,8 +4,11 @@ public class dbtest {
 
     public static void main(String[] args) {
         UserDB db = new UserDB();
-        db.addUser(new User("New", "User", "" + System.currentTimeMillis(),
-                "f0rk$@ndKNIVES33", 't'));
+        db.print();
+        //db.addUser(new User("New", "User", "" + System.currentTimeMillis(),
+                //"f0rk$@ndKNIVES33", 't'));
+        //db.removeUser(db.get("msteve07"));
+        //db.editUserType(db.get("msteve07"), UserTypes.USER_TA);
         db.print();
     }
 }
