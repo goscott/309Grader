cc729ce6a98460693bd9ff83e5850cd1b7df9709
Mason Stevenson <masonjstevenson@gmail.com>
2015-01-27 10:04:19 -0800

    history can load a gradebook_screen.fxml now.

--- src/main/java/controller/history/HistoryController.java
+++ src/main/java/controller/history/HistoryController.java
@@ -1,7 +1,11 @@
 package controller.history;
 
+import java.io.IOException;
+
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 
@@ -21,6 +25,16 @@ public class HistoryController {
     public void switchToGradebook() {
         history_view.setVisible(false);
         gradebook_view.setVisible(true);
+        
+        try {
+            TableView<?> gradebookPage = (TableView<?>) FXMLLoader
+                    .load(getClass().getClassLoader().getResource(
+                            "view/roster/gradebook_screen.fxml"));
+            gradebook_view.setCenter(gradebookPage);
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
     }
     
     public void switchToHistory() {
