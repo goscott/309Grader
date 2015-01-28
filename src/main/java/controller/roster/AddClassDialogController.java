946b86eb2f593da96135c7d92ec46ace358511c5
Gavin Scott <gavin.o.scott@gmail.com>
2015-01-27 22:17:37 -0800

    Typo

--- src/main/java/controller/roster/AddClassDialogController.java
+++ src/main/java/controller/roster/AddClassDialogController.java
@@ -1,6 +1,5 @@
 package controller.roster;
 
-
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -25,81 +24,80 @@ import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
 
-
 /**
  * controls the Add class dialog functionality incomplete
+ * 
  * @author Michael Lenz
  * 
  */
 public class AddClassDialogController {
 
-    @FXML
-    private ResourceBundle resources;
+	@FXML
+	private ResourceBundle resources;
 
-    @FXML
-    private URL location;
+	@FXML
+	private URL location;
 
-    @FXML
-    private Button CancelButton;
+	@FXML
+	private Button CancelButton;
 
-    @FXML
-    private ListView<String> students;
+	@FXML
+	private ListView<String> students;
 
-    @FXML
-    private TextField className;
+	@FXML
+	private TextField className;
 
-    @FXML
-    private Button AddClassButton;
+	@FXML
+	private Button AddClassButton;
 
-    @FXML
-    void initialize() {
-        ObservableList<String> items = FXCollections.observableArrayList ("There will be students here someday");
-        students.setItems(Server.getObserableStudentList());
-        students.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
-     //   students.setItems(items);
-    }
-    
-    @FXML
-    private void AddClass(ActionEvent event) {
-        Roster roster = new Roster(className.getText(), null, 0, null, null, null);
-        roster.Save();
-        Debug.log("roster saved");
-        ((Stage) AddClassButton.getScene().getWindow()).close();
-    }
-    
-    @FXML
-    void Cancel(ActionEvent event) {
-        ((Stage) AddClassButton.getScene().getWindow()).close();
-    }
-    
-    
-    /**
-     * starts the add class dialog
-     * @param stage
-     */
-    public void start(Stage stage) {
+	@FXML
+	void initialize() {
+		ObservableList<String> items = FXCollections
+				.observableArrayList("There will be students here someday");
+		students.setItems(Server.getObserableStudentList());
+		students.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
+		// students.setItems(items);
+	}
 
-        try {
-            Pane page = (Pane) FXMLLoader
-                .load(getClass().getClassLoader().getResource((
-                    "view/roster/addClassDialog.fxml")));
-            Scene popup = new Scene(page);
-            stage.setTitle("Add Class");
-            stage.setScene(popup);
-            stage.setResizable(false);
-            stage.show();
-        } catch (IOException e1) {
-            Debug.log("IO ERROR", "Could not load file to start popup");
-            e1.printStackTrace();
-        }
-        
-        stage.setOnHiding(new EventHandler<WindowEvent>() {
-            public void handle(WindowEvent event) {
-        //        parent.setDisable(false);
-            }
-        });
-    }
-    
-    
-    
+	@FXML
+	private void AddClass(ActionEvent event) {
+		Roster roster = new Roster(className.getText(), null, 0, null, null,
+				null);
+		roster.Save();
+		Debug.log("roster saved");
+		((Stage) AddClassButton.getScene().getWindow()).close();
+	}
+
+	@FXML
+	void Cancel(ActionEvent event) {
+		((Stage) AddClassButton.getScene().getWindow()).close();
+	}
+
+	/**
+	 * starts the add class dialog
+	 * 
+	 * @param stage
+	 */
+	public void start(Stage stage) {
+
+		try {
+			Pane page = (Pane) FXMLLoader.load(getClass().getClassLoader()
+					.getResource(("view/roster/addClassDialog.fxml")));
+			Scene popup = new Scene(page);
+			stage.setTitle("Add Class");
+			stage.setScene(popup);
+			stage.setResizable(false);
+			stage.show();
+		} catch (IOException e1) {
+			Debug.log("IO ERROR", "Could not load file to start popup");
+			e1.printStackTrace();
+		}
+
+		stage.setOnHiding(new EventHandler<WindowEvent>() {
+			public void handle(WindowEvent event) {
+				// parent.setDisable(false);
+			}
+		});
+	}
+
 }
