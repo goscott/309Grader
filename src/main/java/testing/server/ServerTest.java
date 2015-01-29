46e39a11cf5c5324bd28353090a66efa2f9cabef
Shelli <ShelliCrispen@pcp132393pcs.wireless.calpoly.edu>
2015-01-28 22:46:18 -0800

    Testdocs

--- src/main/java/testing/server/ServerTest.java
+++ src/main/java/testing/server/ServerTest.java
@@ -1,5 +1,19 @@
 package testing.server;
 
 public class ServerTest {
+	public void testGetObservableStudentList(){
+		
+	}
+	
+	public void testGetStudentListNotRoster(){
+		
+	}
 
+	public void testGetStudentListName() {
+		
+	}
+	
+	public void testGetStudentListNameNotRoster() {
+		
+	}
 }
