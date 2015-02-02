51ce2cb43e6d895add19ca745cfb312806ca35a1
MasonStevenson <masonjstevenson@gmail.com>
2015-02-02 15:47:47 -0800

    more changes to history.

--- src/main/java/controller/history/HistoryController.java
+++ src/main/java/controller/history/HistoryController.java
@@ -2,12 +2,24 @@ package controller.history;
 
 import java.io.IOException;
 
+import javax.swing.JOptionPane;
+
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.PieChart;
 import javafx.scene.control.Accordion;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TitledPane;
+import javafx.scene.control.ToggleButton;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
@@ -55,14 +67,95 @@ public class HistoryController {
     @FXML
     private BorderPane section_view;
     
+    @FXML
+    private LineChart line_chart;
+    
+    @FXML
+    private PieChart pie_chart;
+    
     private int num = 1;
     
     public void initialize() {
         //load classes from history db
+        class_selector.expandedPaneProperty().addListener(new 
+                ChangeListener<TitledPane>() {
+                    public void changed(ObservableValue<? extends TitledPane> ov, TitledPane old_val, TitledPane new_val) {
+                        if (class_selector.getExpandedPane() != null) {
+                            switchToCourse();
+                        }
+                        
+                        else {
+                            switchToIntro();
+                        }
+                  }
+            });
+        
+        ObservableList<PieChart.Data> pieChartData =
+                FXCollections.observableArrayList(
+                new PieChart.Data("Stuff", 13),
+                new PieChart.Data("Things", 25),
+                new PieChart.Data("Items", 10));
+        pie_chart.setData(pieChartData);
     }
     
     public void addClass() {
-        class_selector.getPanes().add(new TitledPane("Dynamic Pane #" + num++, null));
+        VBox content = new VBox();
+        TitledPane toAdd;
+        ToggleButton placeHolder = new ToggleButton("PlaceHolder");
+        placeHolder.setFocusTraversable(false);
+        
+        placeHolder.setOnAction(new EventHandler<ActionEvent>() {
+
+            @Override
+            public void handle(ActionEvent event) {
+                // TODO Auto-generated method stub
+                if (((ToggleButton)event.getSource()).isSelected()) {
+                    switchToSection();
+                }
+                
+                else {
+                    switchToCourse();
+                }
+            }
+        });
+        
+        content.getChildren().add(placeHolder);
+        toAdd = new TitledPane("Dynamic Pane #" + num++, content);
+        toAdd.setAnimated(true);
+        toAdd.setPadding(new Insets(0, 10, 10, 10));
+        class_selector.getPanes().add(toAdd);
+    }
+    
+    /*
+    public void addSection() {
+        VBox content;
+        
+        for (TitledPane pane : class_selector.getPanes()) {
+            if (pane.getText().equals(targetCourse)) {
+            }
+        }
+    }*/
+    
+    public void switchGraph() {
+        if (line_chart.isVisible()) {
+            switchToPieChart();
+        }
+        
+        else {
+            switchToLineChart();
+        }
+    }
+    
+    public void switchToLineChart() {
+        line_chart.setVisible(true);
+        pie_chart.setVisible(false);
+    }
+    
+    public void switchToPieChart() {
+        line_chart.setVisible(false);
+        pie_chart.setVisible(true);
+        
+        
     }
     
     public void switchToIntro() {
@@ -75,6 +168,8 @@ public class HistoryController {
         course_view.setVisible(true);
         intro_view.setVisible(false);
         section_view.setVisible(false);
+        
+        switchToLineChart();
     }
     
     public void switchToSection() {
