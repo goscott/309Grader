06ea2710a9d79a7307785938dda36f239c35f5dd
Mason Stevenson <masonjstevenson@gmail.com>
2015-02-02 23:03:36 -0800

    History prototype done (uses dummy data for now).

--- src/main/java/controller/history/HistoryController.java
+++ src/main/java/controller/history/HistoryController.java
@@ -17,11 +17,13 @@ import javafx.scene.chart.LineChart;
 import javafx.scene.chart.PieChart;
 import javafx.scene.control.Accordion;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TitledPane;
 import javafx.scene.control.ToggleButton;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Region;
 import javafx.scene.layout.VBox;
 
 /**
@@ -73,19 +75,40 @@ public class HistoryController {
     @FXML
     private PieChart pie_chart;
     
+    @FXML
+    private Label section_label;
+    
+    @FXML
+    private Label course_label;
+    
+    @FXML
+    private Button switch_graph;
+    
     private int num = 1;
+    
+    private ToggleButton selectedSection = null;
     
     public void initialize() {
         //load classes from history db
         class_selector.expandedPaneProperty().addListener(new 
                 ChangeListener<TitledPane>() {
                     public void changed(ObservableValue<? extends TitledPane> ov, TitledPane old_val, TitledPane new_val) {
+                        
+                        //expanded 
                         if (class_selector.getExpandedPane() != null) {
                             switchToCourse();
+                            course_label.setText(class_selector.getExpandedPane().getText() + ": View Under Construction");
+                            
+                            switch_graph.setVisible(true);
                         }
                         
+                        //collapsed
                         else {
                             switchToIntro();
+                            
+                            if (selectedSection != null) {
+                                selectedSection.setSelected(false);
+                            }
                         }
                   }
             });
@@ -96,33 +119,54 @@ public class HistoryController {
                 new PieChart.Data("Things", 25),
                 new PieChart.Data("Items", 10));
         pie_chart.setData(pieChartData);
+        
+        //this is where we would pull info from course history
+        // to build the list of courses
+        addClass();
+        addClass();
+        addClass();
+        addClass();
+        addClass();
+        addClass();
+        addClass();
+        addClass();
+        addClass();
+        addClass();
+        addClass();
+        addClass();
+        addClass();
+        addClass();
+        addClass();
+        addClass();
+        addClass();
+        addClass();
+        addClass();
+        addClass();
+        
+        TitledPane padding = new TitledPane();
+        padding.setVisible(false);
+        class_selector.getPanes().add(padding);
     }
     
     public void addClass() {
         VBox content = new VBox();
         TitledPane toAdd;
-        ToggleButton placeHolder = new ToggleButton("PlaceHolder");
-        placeHolder.setFocusTraversable(false);
+        SectionButton placeHolder = new SectionButton("Section1");
+        SectionButton placeHolder2 = new SectionButton("Section2");
         
-        placeHolder.setOnAction(new EventHandler<ActionEvent>() {
-
-            @Override
-            public void handle(ActionEvent event) {
-                // TODO Auto-generated method stub
-                if (((ToggleButton)event.getSource()).isSelected()) {
-                    switchToSection();
-                }
-                
-                else {
-                    switchToCourse();
-                }
-            }
-        });
         
         content.getChildren().add(placeHolder);
-        toAdd = new TitledPane("Dynamic Pane #" + num++, content);
+        VBox.setMargin(placeHolder, new Insets(0, 0, 10, 0));
+        
+        content.getChildren().add(placeHolder2);
+        VBox.setMargin(placeHolder2, new Insets(0, 0, 10, 0));
+        
+        if (num < 10)
+            toAdd = new TitledPane("CSC/CPE 30" + num++, content);
+        else
+            toAdd = new TitledPane("CSC/CPE 3" + num++, content);
         toAdd.setAnimated(true);
-        toAdd.setPadding(new Insets(0, 10, 10, 10));
+        toAdd.setPadding(new Insets(10, 20, 0, 5));
         class_selector.getPanes().add(toAdd);
     }
     
@@ -203,5 +247,50 @@ public class HistoryController {
     public void switchToHistory() {
         history_view.setVisible(true);
         gradebook_view.setVisible(false);
+    }
+    
+    private class SectionButton extends ToggleButton {
+        
+        public SectionButton() {
+            initialize();
+        }
+        
+        public SectionButton(String name) {
+            super(name);
+            initialize();
+        }
+        
+        public void initialize() {
+            this.setFocusTraversable(false);
+            this.setOnAction(new EventHandler<ActionEvent>() {
+
+                @Override
+                public void handle(ActionEvent event) {
+                    
+                    // on
+                    if (((ToggleButton)event.getSource()).isSelected()) {
+                        switchToSection();
+                        
+                        if (selectedSection != null) {
+                            selectedSection.setSelected(false); //deactivate other button
+                        }
+                        
+                        selectedSection = (ToggleButton) event.getSource();
+                        
+                        section_label.setText(selectedSection.getText() + ": View Under Construction.");
+                        
+                        switch_graph.setVisible(false);
+                    }
+                    
+                    // off
+                    else {
+                        switchToCourse();
+                        selectedSection = null;
+                        switch_graph.setVisible(true);
+                    }
+                }
+            });
+        }
+        
     }
 }
