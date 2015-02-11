e70c1aaace1aaf4da61ea98446a01f5cc696dd9d
Shelli <ShelliCrispen@192.168.33.127>
2015-02-10 22:13:08 -0800

    Test Shelli

---
+++ src/main/java/testing/server/TestSeverJUnit.java
@@ -0,0 +1,20 @@
+package testing.server;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+/**
+ * Test JUint Class for the server. 
+ * @author Shelli Crispen
+ *
+ */
+public class TestSeverJUnit
+{
+
+    @Test
+    public void test()
+    {
+        fail("Not yet implemented");
+    }
+
+}
