056f03adb410698e102c2170345a47e6f605f04e
Gavin Scott <gavin.o.scott@gmail.com>
2015-02-18 09:52:35 -0800

    Got rid of the model.graph package

--- src/main/java/model/graph/JavadocStub.java
+++
@@ -1,9 +0,0 @@
-package model.graph;
-
-/****
-*
-* Generic stub file so javadoc will generate package-level documentation,
-* without any real class files yet defined.
-*
-*/
-public class JavadocStub {}
