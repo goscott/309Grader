d48debae99688ca7554d5a8fa74d1bff8967181d
Mason Stevenson <masonjstevenson@gmail.com>
2015-02-24 21:41:59 -0800

    Moved my test plan into the test java code.

--- src/main/java/testing/administration/UserDBTest.java
+++ src/main/java/testing/administration/UserDBTest.java
@@ -4,6 +4,71 @@ import model.administration.User;
 import model.administration.UserDB;
 import model.administration.UserTypes;
 
+/**
+ * Class UserDBTest is the companion testing class for class UserDb.
+ * 
+ * The UserDB class keeps track of all currently users currently registered for GraderTool. It has several important methods:
+ *      -loadUserDB()
+ *      -updateDB()
+ *      -addUser()
+ *      -removeUser()
+ *      -editUserType()
+ *      -login()
+ *      
+ *  UserDB stores all user info in a file, users.udb. 
+ *  The goal for testing UserDB is to make sure that any time the state of the UserDB changes, 
+ *  this file gets updated, reflects the changes, and has no other data added or removed.
+ *  
+ *  UserDBTest implements the following module test plan:
+ *  
+ *      Phase 1: Unit Test the constructor.
+ *          
+ *  
+ *      Phase 2: Unit Test loadUserDB()
+ *          This private method gets called exactly once when the UserDB is constructed. 
+ *          It takes the information stored in users.udb and populates the UserDB with these initial user objects. 
+ *          Testing for this method should include making sure that the file is opened and closed properly, 
+ *          and that all the data in the file is correctly loaded into the UserDB.
+ *  
+ *      Phase 3: Unit Test updateDB()
+ *          This private method gets called every time the UserDB changes state. 
+ *          It rewrites the users.udb file to reflect the userDB’s current state. 
+ *          To test this method, we must make sure that all the data in the userDB 
+ *          has been written to the file.
+ *          
+ *  
+ *      Phase 4: Unit Test addUser()
+ *          This method adds a new User object to the UserDB. 
+ *          It then calls updateUserDB() to write the changes to the users.udb file. 
+ *          It should add (one and only one) User object if the User is not already in the DB. 
+ *          Users are identified by their unique userID. Tests should determine that the user was 
+ *          added with the correct information.
+ *  
+ *      Phase 5: Unit Test removeUser()
+ *          This method removes a new User object from the UserDB. 
+ *          It then calls updateUserDB() to write the changes to the users.udb file. 
+ *          It should remove one (and only one) User object if the User is in the DB. 
+ *          Users are identified by their unique userID. Tests should determine that the correct user was removed.
+ *  
+ *      Phase 6: Unit Test editUserType()
+ *          This method changes the permissions level for one user. 
+ *          It then calls updateUserDB() to write the changes to the users.udb file.  
+ *          It should change the permissions level of one (and only one) User object if the User is in the DB. 
+ *          Users are identified by their unique userID. Tests should determine that the 
+ *          correct user was edited with the correct permissions level.
+ *  
+ *      Phase 7: Unit Test login()
+ *          This method authenticates a username + password combination to determine if it is in the database or not. 
+ *          It returns the User object if the information is correct and null otherwise. Testing should make sure that 
+ *          1) A user object is only returned if the username + password combination is correct. 2) The correct User 
+ *          object is returned. 3) Otherwise null is returned.
+ *  
+ *      Phase 6: Repeat phases 1 through 5
+ *      
+ *      Phase 7: Stress test by adding and removing 10000 Users
+ * 
+ * @author Mason Stevenson
+ */
 public class UserDBTest {
     UserDB users = new UserDB();
     User me = new User("Mason", "Stevenson", "msteve07", "password", UserTypes.USER_STUDENT);
--- src/main/java/testing/history/CourseHistoryTest.java
+++ src/main/java/testing/history/CourseHistoryTest.java
@@ -1,5 +1,41 @@
 package testing.history;
 
+/**
+ * Class CourseHistoryTest is the companion testing class for class CourseHistory.
+ * 
+ * The CourseHistory class keeps track all Roster objects (different sections) for a single course that are no longer current. It has several important methods:
+ *      -addRoster()
+ *      -removeCourse()
+ *      -hideCourse()
+ *      -unhideCourse()
+ *      
+ * CourseHistoryTest implements the following module test plan:
+ * 
+ *      Phase 1: Unit Test the constructor. 
+ *      
+ *      Phase 2: Unit Test addRoster()
+ *          addRoster adds a new roster to the history, if the roster does not already exist. 
+ *          Testing for this method should check to make sure that only one roster was added, 
+ *          only if is not already in the history.
+ *      
+ *      Phase 3: Unit Test removeCourse()
+ *          RemoveCourse deletes a course from the history.
+ *          It should remove one (and only one) Roster object if the roster is in history or hidden.
+ *      
+ *      Phase 4: Unit Test hideCourse()
+ *          hideCourse moves a course from the history to a collection of roster objects called hidden. 
+ *          It should only move the course if it exists in history.
+ *      
+ *      Phase 5: Unit Test unHideCourse()
+ *          unhideCourse moves a course from hidden back to history. 
+ *          It should only move the course if it exists in hidden.
+ *          
+ *      Phase 6: Repeat phases 1 through 5
+ *      
+ *      Phase 7: Stress test by adding and removing 1000 Rosters
+ *      
+ * @author Mason Stevenson
+ */
 public class CourseHistoryTest {
     
     public void testAddRoster() {
