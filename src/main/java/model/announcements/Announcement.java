3d1a84ee055e86cab6bc7cdec33f6eb52c210c5d
Jacrushar <jrussellh96@gmail.com>
2015-02-25 16:01:40 -0800

    Added some comments or whatever

--- src/main/java/model/announcements/Announcement.java
+++ src/main/java/model/announcements/Announcement.java
@@ -3,36 +3,58 @@ package model.announcements;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
-//here is a basic announcement model class you can build on
+/**
+ * Model Announcement class containing the data for an announcement.
+ * 
+ * @author Jacob Hardi
+ *
+ */
 public class Announcement {
-    private String subject;
-    private String postedBy;
-    private String dateTime; // you should swap this for a Calendar object or something
-    
-    private String content;
-    
-    public Announcement(String newSubject, String newPostedBy, String newContent) {
-    	SimpleDateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy hh:mm a");
-    	Calendar cal = Calendar.getInstance();
-        subject = newSubject;
-        postedBy = newPostedBy;
-        dateTime = dateFormat.format(cal.getTime());
-        content = newContent;
-    }
-    
-    public String getSubject() {
-        return subject;
-    }
-    
-    public String getPostedBy() {
-        return postedBy;
-    }
-    
-    public String getDateTime() {
-        return dateTime;
-    }
-    
-    public String getContent() {
-    	return content;
-    }
+	private String subject;
+	private String postedBy;
+	private String dateTime;
+	private String content;
+
+	/**
+	 * Constructs an Announcement object with the specified subject, poster, and body content.
+	 * @param newSubject The subject of this Announcement.
+	 * @param newPostedBy the name of the user that posted this Announcement.
+	 * @param newContent The body content of this Announcement.
+	 */
+	public Announcement(String newSubject, String newPostedBy, String newContent) {
+		SimpleDateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy hh:mm a");
+		Calendar cal = Calendar.getInstance();
+		subject = newSubject;
+		postedBy = newPostedBy;
+		dateTime = dateFormat.format(cal.getTime());
+		content = newContent;
+	}
+
+	/**
+	 * Returns the subject of this Announcement.
+	 * @return the subject of this Announcement.
+	 */
+	public String getSubject() {
+		return subject;
+	}
+
+	/**
+	 * Returns the name of the user that posted this Announcement.
+	 * @return the name of the user that posted this Announcement.
+	 */
+	public String getPostedBy() {
+		return postedBy;
+	}
+
+	public String getDateTime() {
+		return dateTime;
+	}
+
+	/**
+	 * Returns the body content of this Announcement.
+	 * @return the body content of this Announcement.
+	 */
+	public String getContent() {
+		return content;
+	}
 }
