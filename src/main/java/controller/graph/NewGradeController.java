67bb79ad50bd5a792ae23c03140c7f1339081b31
Shelli <ShelliCrispen@192.168.1.132>
2015-03-02 14:37:12 -0800

    NewGrade Popup

---
+++ src/main/java/controller/graph/NewGradeController.java
@@ -0,0 +1,29 @@
+package controller.graph;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.ColorPicker;
+import javafx.scene.control.TextField;
+import java.awt.event.ActionEvent;
+
+public class NewGradeController {
+
+    @FXML
+    private ColorPicker ColorPicker;
+
+    @FXML
+    private TextField EnterGrade;
+
+    @FXML
+    private TextField EnterPercent;
+
+    @FXML
+    void cancel(ActionEvent event) {
+
+    }
+
+    @FXML
+    void done(ActionEvent event) {
+
+    }
+
+}
