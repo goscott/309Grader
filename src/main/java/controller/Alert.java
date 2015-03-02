ca63fb897aba18f795807ab6cdb73378ac336af7
Gavin Scott <gavin.o.scott@gmail.com>
2015-03-02 15:02:17 -0800

    Generic dialog uses controlsfx now

--- src/main/java/controller/Alert.java
+++ src/main/java/controller/Alert.java
@@ -16,9 +16,8 @@ import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
 /**
- * A generic informational popup to replace
- * JOptionPane, which does not work well with
- * javaFX.
+ * A generic informational popup to replace JOptionPane, which does not work
+ * well with javaFX.
  * 
  * @author Gavin Scott
  */
@@ -26,59 +25,33 @@ import javafx.stage.StageStyle;
 public class Alert {
 	/** The max length of a message **/
 	private static int maxLength = 50;
-	
+
 	/**
-	 * Shows a messasge with the given title and message.
-	 * Messages have a 50 character limit.
+	 * Shows a messasge with the given title and message. Messages have a 50
+	 * character limit.
 	 */
 	public static void show(String message) {
-		if(message.length() > maxLength) {
-			Debug.log("ERROR", "Alert message too long");
-			return;
-		}
-		
-		Stage dialogStage = new Stage(StageStyle.UTILITY);
-		dialogStage.setTitle("Grader");
-		dialogStage.setHeight(150);
-		dialogStage.setWidth(350);
-		dialogStage.setResizable(false);
-		dialogStage.initModality(Modality.WINDOW_MODAL);
-		dialogStage.setScene(new Scene(VBoxBuilder.create().
-		    children(new Text(message)).
-		    alignment(Pos.CENTER).padding(new Insets(5)).build()));
-		GraderPopup.setIcon(dialogStage);
-		dialogStage.show();
+		Dialogs.create().owner(new Stage(StageStyle.UNDECORATED)).title("Grader")
+				.masthead(null).message(message)
+				.showInformation();
 	}
-	
+
 	public static Action showConfirmDialog(String headerMessage, String message) {
-		return Dialogs
-				.create()
-				.owner(new Stage(StageStyle.UNDECORATED))
-				.title("Grader")
-				.masthead(headerMessage)
-				.message(message)
-				.actions(Dialog.ACTION_YES, Dialog.ACTION_NO)
-				.showConfirm();
+		return Dialogs.create().owner(new Stage(StageStyle.UNDECORATED))
+				.title("Grader").masthead(headerMessage).message(message)
+				.actions(Dialog.ACTION_YES, Dialog.ACTION_NO).showConfirm();
 	}
-	
+
 	public static Action showConfirmDialog(String message) {
-		return Dialogs
-				.create()
-				.owner(new Stage(StageStyle.UNDECORATED))
-				.title("Grader")
-				.message(message)
-				.actions(Dialog.ACTION_YES, Dialog.ACTION_NO)
-				.showConfirm();
+		return Dialogs.create().owner(new Stage(StageStyle.UNDECORATED))
+				.title("Grader").message(message)
+				.actions(Dialog.ACTION_YES, Dialog.ACTION_NO).showConfirm();
 	}
 
 	public static Action showWarningDialog(String message) {
-		return Dialogs
-				.create()
-				.owner(new Stage(StageStyle.UNDECORATED))
-				.title("Grader")
-				.masthead("Warning: Cannot be undone")
-				.message(message)
-				.actions(Dialog.ACTION_YES, Dialog.ACTION_NO)
+		return Dialogs.create().owner(new Stage(StageStyle.UNDECORATED))
+				.title("Grader").masthead("Warning: Cannot be undone")
+				.message(message).actions(Dialog.ACTION_YES, Dialog.ACTION_NO)
 				.showWarning();
 	}
 }
