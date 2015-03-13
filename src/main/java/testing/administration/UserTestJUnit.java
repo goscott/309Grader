536b8247be7b0e64a3b13f95c943f91119806efb
MasonStevenson <masonjstevenson@gmail.com>
2015-03-13 14:06:15 -0700

    Test Plans.

---
+++ src/main/java/testing/administration/UserTestJUnit.java
@@ -0,0 +1,28 @@
+package testing.administration;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+* Class UserTestJUnit is the companion testing class for class User.
+*      
+* UserTestJUnit implements the following module test plan:
+* 
+*      Phase 1: Unit Test the constructor.
+*      
+*      Phase 2: Unit Test editType()
+*      
+*      Phase 3: Unit Test equals()
+*      
+*      Phase 4: Unit addPermission()
+*      
+*      Phase 5: Unit Test setPermission()
+*      
+*      Phase 6: Unit Test setPermissions()
+*      
+* @author Mason Stevenson
+*/
+public class UserTestJUnit {
+
+}
---
+++ src/main/java/testing/history/HistoryDBTestJUnit.java
@@ -0,0 +1,24 @@
+package testing.history;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+* Class HistoryDBTestJUnit is the companion testing class for class User.
+*      
+* HistoryDBTestJUnit implements the following module test plan:
+* 
+*      Phase 1: Unit Test addRoster()
+*      
+*      Phase 2: Unit Test hasCourse()
+*    
+*      Phase 3: Unit Test getCourseHistory()
+*      
+*      Phase 4: Unit Test save()
+*      
+* @author Mason Stevenson
+*/
+public class HistoryDBTestJUnit {
+
+}
