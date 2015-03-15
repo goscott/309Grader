5973d5dfe76dd3902d0aff4009676ff752c17c05
Gavin Scott <gavin.o.scott@gmail.com>
2015-03-14 21:57:26 -0700

    Fixed Histo bug

--- src/main/java/controller/graph/GradeShape.java
+++ src/main/java/controller/graph/GradeShape.java
@@ -158,7 +158,7 @@ public class GradeShape extends Rectangle {
 	}
 
 	private double getScoreFromLocation(double y) {
-		return ((y + Histogram.BAR_WIDTH / 2 - Histogram.TOP_BUFFER)
+		return 1 + ((y + Histogram.BAR_WIDTH / 2 - Histogram.TOP_BUFFER)
 						/ Histogram.BAR_WIDTH - Histogram.NUM_TICKS) * -1;
 	}
 
