1388fc6f1dad8928c8c267d60e6cf53927810639
Michael <mchimp@hotmail.com>
2015-03-17 15:09:51 -0700

    Tests

--- src/main/java/testing/server/TestSeverJUnit.java
+++ src/main/java/testing/server/TestSeverJUnit.java
@@ -1,7 +1,11 @@
 package testing.server;
 
 import static org.junit.Assert.*;
+import javafx.collections.ObservableList;
+import model.roster.Student;
+import model.server.Server;
 
+import org.junit.Before;
 import org.junit.Test;
 /**
  * Test JUint Class for the server. 
@@ -10,7 +14,13 @@ import org.junit.Test;
  */
 public class TestSeverJUnit
 {
-
+    
+    @Before
+    public void setUp()
+    {
+        Server.init();
+    }
+    
     @Test
     public void test()
     {
@@ -19,21 +29,25 @@ public class TestSeverJUnit
 
     @Test
     public void testGetObservableStudentList(){
-        
+        ObservableList<Student> testList = Server.getObservableStudentList();        
+        assertTrue("Server does not have a list of students",testList != null);
     }
     
     @Test
     public void testGetStudentListNotRoster(){
-        
+        ObservableList<Student> testList =  Server.getStudentListNotRoster();        
+        assertTrue("Server did not return a list of students not in roster", testList != null);
     }
 
     @Test
     public void testGetStudentListName() {
-        
+        ObservableList<String> testList =  Server.getStudentListName();        
+        assertTrue("Server did not return a list of student names", testList != null);
     }
     
     @Test
     public void testGetStudentListNameNotRoster() {
-        
+        ObservableList<String> testList = Server.getStudentListNameNotRoster();
+        assertTrue("Server did not return a list of student names not in roster", testList != null);
     }
 }
