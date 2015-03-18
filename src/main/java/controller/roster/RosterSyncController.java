76307475f181e8a76fe070aed3d5d1a364dfa979
Gavin Scott <gavin.o.scott@gmail.com>
2015-03-18 00:28:08 -0700

    added shelli's controller (roster synch)

---
+++ src/main/java/controller/roster/RosterSyncController.java
@@ -0,0 +1,104 @@
+package controller.roster;
+
+import java.util.ArrayList;
+
+import javafx.scene.control.ListCell;
+import model.administration.UserTypes;
+import model.driver.Grader;
+import model.roster.Student;
+import model.server.Server;
+import controller.GraderPopup;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.geometry.Orientation;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
+import javafx.stage.Stage;
+import javafx.util.Callback;
+
+import java.util.Set;
+
+/**
+ * This is the RosterSync Controller 
+ * @author Shelli Crispen
+ *
+ */
+public class RosterSyncController
+{
+    @FXML 
+    private ListView<String> syncLocal;
+    /** The Servers roster list **/
+    @FXML 
+    private ListView<String> syncServer;
+    /** Button to sync the rosters **/
+    @FXML
+    private Button syncButton;
+    /** The controller for the gradebook **/
+    private static GradebookController gbook;
+    /** ArrayList of local names to be deleted **/
+    private ArrayList<Student> localArray;
+    /** ArrayList of server names to be added **/
+    private ArrayList<Student> serverArray;
+    /** Spacing used for list View **/
+    private static final String SPACING = " %-36s%35s";
+
+    
+
+    
+    /**
+     * Initializes the dropdown box.
+     */
+    public void initialize() {
+        gbook = GradebookController.get();
+        ObservableList<String> server =  FXCollections.observableArrayList();   
+        ObservableList<String> local =  FXCollections.observableArrayList();  
+        
+        for(Student stu: Grader.getRoster().rosterSync(false))
+        {
+            serverArray.add(stu);
+            server.add(String.format(SPACING, stu.getName()));
+        }
+        for(Student stu: Grader.getRoster().rosterSync(true))
+        {
+            localArray.add(stu);
+            local.add(String.format(SPACING, stu.getName()));
+        }   
+        
+        syncLocal.setItems(local);
+        syncLocal.setOrientation(Orientation.HORIZONTAL);
+        syncServer.setItems(server);
+        syncServer.setOrientation(Orientation.HORIZONTAL);   
+    }
+    
+
+    /**
+     * Creates the popup on the given stage
+     * @param stage the stage
+     */
+    public void start(Stage stage) {
+        GraderPopup.getPopupStage("Roster Sync", "view/mainpage/RosterSync.fxml")
+                .show();
+    }
+
+    /**
+     * Handles the Sync button. Closes the window.
+     * @param event the button's event
+     */
+    @FXML
+    private void handleSyncButton(ActionEvent event) {
+        for(Student stu: serverArray)
+        {
+            Grader.getRoster().addStudent(stu);
+        }
+        for(Student stu: localArray)
+        {
+            Grader.getRoster().dropStudent(stu);
+        }
+    }
+
+    
+}
