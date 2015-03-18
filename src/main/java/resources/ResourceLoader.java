38aaa49b31dffcb50fc4a091b24f995538cf67d0
Gavin Scott <gavin.o.scott@gmail.com>
2015-03-18 16:55:08 -0700

    style

--- src/main/java/resources/ResourceLoader.java
+++ src/main/java/resources/ResourceLoader.java
@@ -7,7 +7,7 @@ import javafx.scene.image.Image;
 /**
  * Standardizes coloring and resource loading
  * 
- * @author Gavin Scott 
+ * @author Gavin Scott
  * @author Shelli Crispen
  */
 public class ResourceLoader {
@@ -21,21 +21,27 @@ public class ResourceLoader {
 	public final static Color ORANGE = Color.decode("#FFBC7F");
 	/** The default red used by the program **/
 	public final static Color RED = Color.decode("#FE7E84");
-	
+
 	/** The red used to indicate an error in user input **/
-	public final static javafx.scene.paint.Color ERROR_RED = javafx.scene.paint.Color.web("#FE7E84");
+	public final static javafx.scene.paint.Color ERROR_RED = javafx.scene.paint.Color
+			.web("#FE7E84");
 	/** The white used to fill fields to indicate no error **/
 	public final static javafx.scene.paint.Color NOERROR_WHITE = javafx.scene.paint.Color.WHITE;
 
 	/** The color of a line in the histogram **/
-	public final static javafx.scene.paint.Color LINE_COLOR = javafx.scene.paint.Color.web("#03434B");
+	public final static javafx.scene.paint.Color LINE_COLOR = javafx.scene.paint.Color
+			.web("#03434B");
 	/** The color of a bar in the histogram **/
-    public final static javafx.scene.paint.Color BAR_COLOR = javafx.scene.paint.Color.web("#BC7BE9");
-    /** The color of the histogram's background **/
-    public final static javafx.scene.paint.Color BACKGROUND = javafx.scene.paint.Color.web("#CBEFF4");
+	public final static javafx.scene.paint.Color BAR_COLOR = javafx.scene.paint.Color
+			.web("#BC7BE9");
+	/** The color of the histogram's background **/
+	public final static javafx.scene.paint.Color BACKGROUND = javafx.scene.paint.Color
+			.web("#CBEFF4");
 
 	/** The program's icon, wich is displayed in the status bar of most popups **/
-	public final static Image ICON = new Image(ResourceLoader.class.getResourceAsStream( "GraderIcon.png" ));
+	public final static Image ICON = new Image(
+			ResourceLoader.class.getResourceAsStream("GraderIcon.png"));
 	/** The program's load screen **/
-	public final static Image LOAD_SCREEN = new Image(ResourceLoader.class.getResourceAsStream( "GraderLoadScreen.jpg" ));
+	public final static Image LOAD_SCREEN = new Image(
+			ResourceLoader.class.getResourceAsStream("GraderLoadScreen.jpg"));
 }
