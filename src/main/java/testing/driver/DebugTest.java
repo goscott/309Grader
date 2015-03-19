9bc3425c17a9adb205fc22afe2a882d6e7c8ea41
Shelli <ShelliCrispen@192.168.1.132>
2015-03-18 18:43:35 -0700

    Test Plans

--- src/main/java/testing/driver/DebugTest.java
+++ src/main/java/testing/driver/DebugTest.java
@@ -1,5 +1,21 @@
 package testing.driver;
 
+/**
+ * 
+ * @author Shelli Crispen
+ *  Phase 1: Unit Test constructor()
+ *      Tests that the Debugger sets up correctly.
+ *      
+ *  Phase 2: Unit Test test()
+ *      Tests that the log functions work correctly
+ *  
+ *  Phase 3: Unit Test testAutoPopulate()
+ *      Tests that the autoPopulate function correctly pulls information
+ *  
+ *  Phase 4: Unit Test testNewFile()
+ *      Tests that the program goes through the process of creating a new file.
+ *
+ */
 public class DebugTest {
 
 }
